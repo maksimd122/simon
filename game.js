337741a34fdba1c7553eb15be2a68c1c3a32b309
maksimd122 <maksimd122@gmail.com>
2022-04-16 01:11:26 +0300

    New files

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ function checkAnswer(currentLevel) {
             userClickedPattern = [];
             nextSequence();
         });
-        $('body').bind('swipeleft', function() {
+        $('body').on('swipeleft', function() {
             $("body").unbind();
             level = 0;
             gamePattern = [];
@@ -70,7 +70,7 @@ $("body").keydown(function () {
     });
 });
 
-$('body').bind('swipeleft', function() {
+$('body').on('swipeleft', function() {
     $("body").unbind();
     nextSequence();
     $(".btn").click(function () {
