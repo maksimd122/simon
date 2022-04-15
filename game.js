6d87c714d9077c3ebda4396a7ff30258bac8f2d2
maksimd122 <54248887+maksimd122@users.noreply.github.com>
2022-04-16 00:32:14 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ function checkAnswer(currentLevel) {
             userClickedPattern = [];
             nextSequence();
         });
-        $('body').bind('touchend', function() {
+        $('body').bind('swipeleft', function() {
             $("body").unbind();
             level = 0;
             gamePattern = [];
@@ -70,7 +70,7 @@ $("body").keydown(function () {
     });
 });
 
-$('body').bind('touchend', function() {
+$('body').bind('swipeleft', function() {
     $("body").unbind();
     nextSequence();
     $(".btn").click(function () {
