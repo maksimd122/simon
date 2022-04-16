4a403a77cf30744097397a5db40340f8f4932abf
maksimd122 <maksimd122@gmail.com>
2022-04-16 14:18:08 +0300

    Mobile support 3.0

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ function checkAnswer(currentLevel) {
             userClickedPattern = [];
             nextSequence();
         });
-        $('.restart').bind("touchstart", function () {
+        $('.restart').bind("tap", function () {
             $(".restart").unbind();
             level = 0;
             gamePattern = [];
@@ -70,7 +70,7 @@ $("body").keydown(function () {
     });
 });
 
-$('.restart').bind("touchstart", function () {
+$('.restart').bind("tap", function () {
     $(".restart").unbind();
     nextSequence();
     $(".btn").click(function () {
